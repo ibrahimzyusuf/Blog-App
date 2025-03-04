a97fd858528168e13e62466bcba84f7a229c7730
ibrahimzyusuf <ibramhashalyusuf880@gmail.com>
2025-03-04 17:28:41 +0300

    returning the user avatar from local

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,7 +29,7 @@ const UserSchema=new mongoose.Schema(
         profilePhoto:{
             type:Object,
             default:{
-                url:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+                url:'../../frontend/public/images/user-avatar.png',
                 publicId:null,
             }
         },
