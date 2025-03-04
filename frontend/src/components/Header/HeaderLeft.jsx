b3c6bbd9299706b19f5fd4bfc89b5ed37737f25c
ibrahimzyusuf <ibramhashalyusuf880@gmail.com>
2025-03-04 11:00:19 +0300

    fixing className

--- frontend/src/components/Header/HeaderLeft.jsx
+++ frontend/src/components/Header/HeaderLeft.jsx
@@ -5,7 +5,7 @@ return (
     <div className="header-left">
         <div className="header-logo">
             <strong>BLOG</strong>
-            <i class="bi bi-pencil"></i>
+            <i className="bi bi-pencil"></i>
         </div>
         <div onClick={()=>settoggle(prev=>!prev)} className="header-menu">
             {toggle?<i className="bi bi-x-lg"></i>:<i className="bi bi-list"></i>}
